b003ca0b43a383c3180d6b2346c3fe0feb6dd7a9
Karolis <keptup@users.noreply.github.com>
2019-03-06 19:27:36 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -14,81 +14,86 @@ let resetState = false;
 let intervalId;
 
 const toReadableConverter = (ms) => {
-  let milliseconds = parseInt(ms % 1000);
-	let seconds = parseInt((ms / 1000 * 1) % 60);
+  let milliseconds = parseInt(ms%1000);
+  let seconds = parseInt((ms / 1000 * 1) % 60);
 	let minutes = parseInt((ms / (1000 * 60)) % 60);
+	let hours = parseInt((ms / (1000 * 60 * 60)) % 24);
 
+	hours = hours < 10 ? '0' + hours : hours;
 	minutes = minutes < 10 ? '0' + minutes : minutes;
 	seconds = seconds < 10 ? '0' + seconds : seconds;
 	if (milliseconds < 10) {
-		milliseconds = '00' + milliseconds;
+	   milliseconds = '00' + milliseconds;
 	} else if (milliseconds < 100) {
-		milliseconds = '0' + milliseconds;
-	} else {
-		milliseconds;
+	   milliseconds = '0' + milliseconds;
+   } else {
+	   milliseconds;
 	};
 
-	return `${minutes}:${seconds}`;
-};
-const countDownClock = (setTime, startButtonClickTime) => {
-  difference = stopButtonClickTime === 0 ? setTime + startButtonClickTime - new Date().getTime() : stopButtonClickTime + startButtonClickTime - new Date().getTime();
-  clock.textContent = toReadableConverter(difference);
-  if (difference <= 10000) {
+	const timeFormat = hours > 0 ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`;
+	return timeFormat;
+  };
+const countDownClock = (setTimeParam, startButtonClickTimeParam) => {
+  difference = stopButtonClickTime === 0 ? setTimeParam + startButtonClickTimeParam - new Date().getTime() : setTimeParam + startButtonClickTimeParam - new Date().getTime();
+	clock.textContent = toReadableConverter(difference);
+	if (difference <= 10200 && difference > 100) {
     audio.play();
-  };
-  if (difference < 0) {
+	};
+	if (difference < 0) {
     clockOn = false;
-    clearInterval(intervalId);
-  };
+		clearInterval(intervalId);
+	};
 
-  return difference;
+	return difference;
 };
 
 plus.addEventListener('click', () => {
-  if (!clockOn && stopButtonClickTime === 0) {
+	if (!clockOn && stopButtonClickTime === 0) {
     setTime += 60000;
-    clock.textContent = toReadableConverter(setTime);
-  } else if (!clockOn && difference > 0) {
-    stopButtonClickTime += 60000;
-    setTime = stopButtonClickTime;
-    clock.textContent = toReadableConverter(setTime);
-  }
+		clock.textContent = toReadableConverter(setTime);
+	} else if (!clockOn && difference > 0) {
+		stopButtonClickTime += 60000;
+		setTime = stopButtonClickTime;
+		clock.textContent = toReadableConverter(setTime);
+	};
+
+	stop.textContent = 'RESET';
 });
 minus.addEventListener('click', () => {
-  if (!clockOn && setTime >= 60000 && difference === 0) {
+	if (!clockOn && setTime >= 60000 && difference === 0) {
     setTime -= 60000;
-    clock.textContent = toReadableConverter(setTime);
-  } else if (!clockOn && setTime >= 60000 && difference >= 60000) {
-    stopButtonClickTime -= 60000;
-    setTime = stopButtonClickTime;
-    clock.textContent = toReadableConverter(setTime);
-  }
+		clock.textContent = toReadableConverter(setTime);
+	} else if (!clockOn && setTime >= 60000 && difference >= 60000) {
+		stopButtonClickTime -= 60000;
+		setTime = stopButtonClickTime;
+		clock.textContent = toReadableConverter(setTime);
+	}
 });
 start.addEventListener('click', () => {
-  if (!clockOn) {
+  if (!clockOn && setTime !== 0) {
     clockOn = !clockOn;
-    startButtonClickTime = new Date().getTime();
-    intervalId = setInterval(countDownClock, 100, setTime, startButtonClickTime);
+		startButtonClickTime = new Date().getTime();
+		intervalId = setInterval(countDownClock, 100, setTime, startButtonClickTime);
 
-    resetState = false;
-    stop.textContent = 'STOP';
-  }
+		resetState = false;
+		stop.textContent = 'STOP';
+	}
 });
 stop.addEventListener('click', () => {
-  if (!resetState && difference !== 0) {
+	if (!resetState && difference !== 0) {
     clockOn = !clockOn;
-    stopButtonClickTime = difference;
-    clearInterval(intervalId);
+		stopButtonClickTime = difference;
+		clearInterval(intervalId);
 
-    resetState = !resetState;
-    stop.textContent = 'RESET';
+		resetState = !resetState;
+		stop.textContent = 'RESET';
   } else {
-    clockOn = false;
-    resetState = false;
-    stop.textContent = 'STOP';
-    stopButtonClickTime = 0;
-    setTime = 0;
-    difference = 0;
-    clock.textContent = toReadableConverter(setTime);
-  }
+		clockOn = false;
+		resetState = false;
+		stop.textContent = 'STOP';
+		stopButtonClickTime = 0;
+		setTime = 0;
+		difference = 0;
+		clock.textContent = toReadableConverter(setTime);
+	}
 });
